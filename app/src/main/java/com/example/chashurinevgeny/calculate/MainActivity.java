e96578e992ed047fe09e68bd9c1c4fac1347ea95
Supsane <echashurin@gmail.com>
2017-04-08 23:53:37 +0300

    Add Component in MainActivity

--- app/src/main/java/com/example/chashurinevgeny/calculate/MainActivity.java
+++ app/src/main/java/com/example/chashurinevgeny/calculate/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.chashurinevgeny.calculate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView enterTextView = null;
+    private TextView resultTextView = null;
+    private Button oneButton = null;
+    private Button twoButton = null;
+    private Button threeButton = null;
+    private Button fourButton = null;
+    private Button fiveButton = null;
+    private Button sixButton = null;
+    private Button sevenButton = null;
+    private Button eightButton = null;
+    private Button nineButton = null;
+    private Button zeroButton = null;
+    private Button dotButton = null;
+    private Button resultButton = null;
+    private Button addButton = null;
+    private Button subtractionButton = null;
+    private Button multiplicationButton = null;
+    private Button divisionButton = null;
+    private Button resetButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
