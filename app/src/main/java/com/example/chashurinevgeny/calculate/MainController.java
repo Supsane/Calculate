d7637dce8e32ccec92e949984bf37749552defaa
Supsane <echashurin@gmail.com>
2017-04-11 07:18:45 +0300

    add checkEnterTextView

--- app/src/main/java/com/example/chashurinevgeny/calculate/MainController.java
+++ app/src/main/java/com/example/chashurinevgeny/calculate/MainController.java
@@ -11,18 +11,32 @@ import android.widget.TextView;
 public class MainController {
     public String countResult(String enterTextView, Context context) {
         String[] numberArray;
+        String result = "0";
+        float number1;
+        float number2;
         for (int i = 0; i < enterTextView.length(); i++) {
             if (String.valueOf(enterTextView.charAt(i)).equals(context.getResources().getText(R.string.addButton).toString())) {
-                numberArray = enterTextView.split(context.getResources().getText(R.string.addButton).toString());
+//                numberArray = enterTextView.split(context.getResources().getText(R.string.addButton).toString());
+//                number1 = Integer.parseInt(numberArray[0]);
+//                number2 = Integer.parseInt(numberArray[1]);
+//                result = String.valueOf(number1 + number2);
             } else if (String.valueOf(enterTextView.charAt(i)).equals(context.getResources().getText(R.string.subtractionButton).toString())) {
                 numberArray = enterTextView.split(context.getResources().getText(R.string.subtractionButton).toString());
+                number1 = Float.parseFloat(numberArray[0]);
+                number2 = Float.parseFloat(numberArray[1]);
+                result = String.valueOf(number1 - number2);
             } else if (String.valueOf(enterTextView.charAt(i)).equals(context.getResources().getText(R.string.multiplicationButton).toString())) {
                 numberArray = enterTextView.split(context.getResources().getText(R.string.multiplicationButton).toString());
             } else if (String.valueOf(enterTextView.charAt(i)).equals(context.getResources().getText(R.string.divisionButton).toString())) {
                 numberArray = enterTextView.split(context.getResources().getText(R.string.divisionButton).toString());
+                number1 = Float.parseFloat(numberArray[0]);
+                number2 = Float.parseFloat(numberArray[1]);
+                if (number2 == 0) {
+                    result = "На ноль делить нельзя";
+                }
+                result = String.valueOf(number1 / number2);
             }
         }
-        String result = "check";
         return result;
     }
 
